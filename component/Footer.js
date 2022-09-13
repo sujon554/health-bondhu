d2466b5e453669a2c48012a137c19f525f31db95
sujon554 <mr.sujon554@gmail.com>
2022-09-13 22:37:01 +0600

    final

--- component/Footer.js
+++ component/Footer.js
@@ -1,44 +1,75 @@
 import { Col, Container, Image, Row } from "react-bootstrap";
 
-
 const Footer = () => {
-    return (
-        <Container>
-            <Row>
-                <Col className="col-md-4">
-                    <h1>Doc<span>mic</span></h1>
-                    <p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has bee</p>
-                    <Image src='/images/Group 31.png' alt="" />
-<br></br>
-                    <small>Copyright @222 Medicom All Rights Reserved</small>
-                </Col>
-                <Col className="col-md-2">
-                    <h4>Quick Links</h4>
-                    <p><a>Our Pricing</a></p>
-                    <p><a>Our Gallery</a></p>
-                    <p><a>Appointment</a></p>
-                    <p><a>Privacy Policy</a></p>
-                </Col>
-                <Col className="col-md-2">
-                   
-                <br></br>
-                <p><a>Services</a></p>
-                <p><a>Our Doctors</a></p>
-                <p><a>Our Latest News</a></p>
-                <p><a>Careers</a></p>
-                <p><a>Contacts Us</a></p>
-                </Col>
-                <Col className="col-md-4">
-                    <h4>Opening Hours</h4>
-                    <p><a>Mon- Tue</a>08:00 AM- 05:00 PM</p>
-                    <p><a>Wed- Thu</a>09:00 AM- 06:00 PM</p>
-                    <p><a>Fri- Sat</a>10:00 AM- 07:00 PM</p>
-                    <p><a>Sunday</a>Emergency Only</p>
-                    <p><a>Personal</a>Mon- 05:00 PM</p>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Row className="my-5">
+        <Col className="col-md-4">
+          <h1>
+            Doc<span>mic</span>
+          </h1>
+          <p>
+            simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has bee
+          </p>
+          <Image src="/images/Group 31.png" alt="" />
+          <br></br>
+          <small>Copyright @222 Medicom All Rights Reserved</small>
+        </Col>
+        <Col className="col-md-2">
+          <h4>Quick Links</h4>
+          <p>
+            <a>Our Pricing</a>
+          </p>
+          <p>
+            <a>Our Gallery</a>
+          </p>
+          <p>
+            <a>Appointment</a>
+          </p>
+          <p>
+            <a>Privacy Policy</a>
+          </p>
+        </Col>
+        <Col className="col-md-2">
+          <br></br>
+          <p>
+            <a>Services</a>
+          </p>
+          <p>
+            <a>Our Doctors</a>
+          </p>
+          <p>
+            <a>Our Latest News</a>
+          </p>
+          <p>
+            <a>Careers</a>
+          </p>
+          <p>
+            <a>Contacts Us</a>
+          </p>
+        </Col>
+        <Col className="col-md-4">
+          <h4>Opening Hours</h4>
+          <p>
+            <a>Mon- Tue</a>08:00 AM- 05:00 PM
+          </p>
+          <p>
+            <a>Wed- Thu</a>09:00 AM- 06:00 PM
+          </p>
+          <p>
+            <a>Fri- Sat</a>10:00 AM- 07:00 PM
+          </p>
+          <p>
+            <a>Sunday</a>Emergency Only
+          </p>
+          <p>
+            <a>Personal</a>Mon- 05:00 PM
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Footer;
--- component/Testimonial.js
+++ component/Testimonial.js
@@ -5,44 +5,76 @@ import styles from "../styles/Testimonial.module.css";
 const Testimonial = () => {
   const settings = {
     dots: true,
-      lazyLoad: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      initialSlide: 2,
+    lazyLoad: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 2,
   };
-
 
   return (
     <Container>
-      <Row>
-        <h1>Thi s is TEst monial</h1>
-        
-        <Slider {...settings}>
-          <div>
-            <img src="https://s3.amazonaws.com/static.neostack.com/img/react-slick/abstract03.jpg" />
+      <Row className="my-5 align-items-center justify-content-center">
+        <h5 className={styles.years}>Testimonial</h5>
+        <h1 className={styles.say}>What they say?</h1>
+        <Slider className="align-items-center justify-content-center" {...settings}>
+          <div className="w-50 h-25">
+            <Row className="m-5 ">
+              <Col className="col-8">
+                <h5>David Jeans</h5>
+                <h6>Pratient</h6>
+                <p>
+                  simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industrys standard dummy text ever
+                  since the 1500s, when an unknown printer took a galley of type
+                  and scrambled it is a long established fact that a reader will
+                  be distracted by the readable content{" "}
+                </p>
+              </Col>
+              <Col className="col-4">
+                <Image src="/images/testi.PNG" alt="" />
+              </Col>
+            </Row>
           </div>
-          <div>
-            <img
-              src="https://s3.amazonaws.com/static.neostack.com/img/react-slick/abstract03.jpg"
-              alt=""
-            />
+          <div className="w-50 h-25">
+            <Row className="m-5">
+              <Col className="col-8">
+                <h5>David Jeans</h5>
+                <h6>Pratient</h6>
+                <p >
+                  simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industrys standard dummy text ever
+                  since the 1500s, when an unknown printer took a galley of type
+                  and scrambled it is a long established fact that a reader will
+                  be distracted by the readable content{" "}
+                </p>
+              </Col>
+              <Col className="col-4">
+                <Image src="/images/testi.PNG" alt="" />
+              </Col>
+            </Row>
           </div>
-          <div>
-            <img
-              src="https://s3.amazonaws.com/static.neostack.com/img/react-slick/abstract03.jpg"
-              alt=""
-            />
+          <div className="w-50 h-25">
+            <Row className="m-5">
+              <Col className="col-8">
+                <h5>David Jeans</h5>
+                <h6>Pratient</h6>
+                <p>
+                  simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industrys standard dummy text ever
+                  since the 1500s, when an unknown printer took a galley of type
+                  and scrambled it is a long established fact that a reader will
+                  be distracted by the readable content{" "}
+                </p>
+              </Col>
+              <Col className="col-4">
+                <Image src="/images/testi.PNG" alt="" />
+              </Col>
+            </Row>
           </div>
-          <div>
-            <img
-              src="https://s3.amazonaws.com/static.neostack.com/img/react-slick/abstract03.jpg"
-              alt=""
-            />
-          </div>
+          
         </Slider>
-       
       </Row>
     </Container>
   );
